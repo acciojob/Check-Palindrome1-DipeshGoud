54ddeb9f191108162326346b90c4697881a39152
DipeshGoud <112858278+DipeshGoud@users.noreply.github.com>
2023-05-21 13:51:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,23 @@
 
 function palindrome(str){
 
+	let i=0;
+	let j=str.length-1;
+	while(i<=j){
+		if (strs[i]===" ") {
+			i++;
+		}
+		if (str[j]===" ") {
+			j--;
+		}
+
+		if(str[i]===str[j]){
+			i++;
+			j--;
+		}else{
+			return false;
+		}
+	}
+	return true;
 }
 module.exports = palindrome
